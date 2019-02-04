e18c62f275a1a558585fc378ce728d784a0542a6
ctamas <ballin360x@gmail.com>
2019-02-04 02:53:47 +0000

    Updates

---
+++ precache-manifest.3e4786cb30105e0b9fba93da2582e5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae7c3512697c7c535bc433919e913d62",
+    "url": "/hackernewsStories/static/media/news.ae7c3512.png"
+  },
+  {
+    "revision": "b5ca009eef879a9401e8",
+    "url": "/hackernewsStories/static/js/runtime~main.b5ca009e.js"
+  },
+  {
+    "revision": "8549dcbf9c499442cbd1",
+    "url": "/hackernewsStories/static/js/main.8549dcbf.chunk.js"
+  },
+  {
+    "revision": "8b0bb45d0fa4ec4633bd",
+    "url": "/hackernewsStories/static/js/1.8b0bb45d.chunk.js"
+  },
+  {
+    "revision": "8549dcbf9c499442cbd1",
+    "url": "/hackernewsStories/static/css/main.ea300576.chunk.css"
+  },
+  {
+    "revision": "1acb688f41cf49835e2e3bc06c2e8325",
+    "url": "/hackernewsStories/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hackernewsStories/precache-manifest.4cb23fdfc1db3eb3f730f527149e3a44.js"
+  "/hackernewsStories/precache-manifest.3e4786cb30105e0b9fba93da2582e5bc.js"
 );
 
 workbox.clientsClaim();
